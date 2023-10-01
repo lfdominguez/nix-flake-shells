5b876116b05b2d09458b4ac3e8548c6232cb0c69
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2023-10-01 14:13:36 -0300

    Test platformio output

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+    description = "Set of my owns Shells";
+
+    outputs = { self, nixpkgs }:
+    let
+        system = "x86_64-linux";
+
+        pkgs = import nixpkgs {
+            inherit system;
+
+            config.allowUnfress = true;
+        };
+    in {
+        platformio = import ./platformio.nix {};
+    };
+}
