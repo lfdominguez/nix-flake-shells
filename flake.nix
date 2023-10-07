c9abd057f869d5ac0b051be94ff52a429aee95ad
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2023-10-07 14:45:31 -0300

    Add shell 'with_openssl_headers'
    Signed-off-by: Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
           let pkgs = nixpkgs.legacyPackages.${system}; in
           {
             devShells.platformio = import ./shells/platformio.nix { inherit pkgs; };
+            devShells.with_openssl_headers = import ./shells/with_openssl_headers.nix { inherit pkgs; };
           }
         );
 }
