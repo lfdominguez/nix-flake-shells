d49c02d00ee040245072cb6e4f6b1ab0064a9652
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2023-10-28 20:07:59 -0300

    Add flutter
    Signed-off-by: Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
           {
             devShells.platformio = import ./shells/platformio.nix { inherit pkgs; };
             devShells.with_openssl_headers = import ./shells/with_openssl_headers.nix { inherit pkgs; };
+            devShells.flutter = import ./shells/flutter.nix { inherit pkgs; };
           }
         );
 }
