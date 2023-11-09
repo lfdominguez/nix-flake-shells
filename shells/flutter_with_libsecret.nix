44401631f141ee3ec451af045ea256d470858f93
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2023-11-09 00:55:45 -0300

    Add libepoxy to flutter with libsecret shell
    Signed-off-by: Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>

--- shells/flutter_with_libsecret.nix
+++ shells/flutter_with_libsecret.nix
@@ -20,9 +20,10 @@ mkShell {
     libgpg-error
     xorg.libXdmcp
     libxkbcommon
+    libepoxy
   ];
 
   shellHook = ''
-    export PKG_CONFIG_PATH=${libxkbcommon.dev}/lib/pkgconfig:${xorg.libXdmcp.dev}/lib/pkgconfig:${libgpg-error.dev}/lib/pkgconfig:${libdatrie.dev}/lib/pkgconfig:${libgcrypt.dev}/lib/pkgconfig:${libthai.dev}/lib/pkgconfig:${pcre.dev}/lib/pkgconfig:${libsepol}/lib/pkgconfig:${libselinux.dev}/lib/pkgconfig:${util-linux.dev}/lib/pkgconfig:${gtk3.dev}/lib/pkgconfig:${pcre2.dev}/lib/pkgconfig:${libsecret.dev}/lib/pkgconfig:$PKG_CONFIG_PATH
+    export PKG_CONFIG_PATH=${libepoxy.dev}/lib/pkgconfig:${libxkbcommon.dev}/lib/pkgconfig:${xorg.libXdmcp.dev}/lib/pkgconfig:${libgpg-error.dev}/lib/pkgconfig:${libdatrie.dev}/lib/pkgconfig:${libgcrypt.dev}/lib/pkgconfig:${libthai.dev}/lib/pkgconfig:${pcre.dev}/lib/pkgconfig:${libsepol}/lib/pkgconfig:${libselinux.dev}/lib/pkgconfig:${util-linux.dev}/lib/pkgconfig:${gtk3.dev}/lib/pkgconfig:${pcre2.dev}/lib/pkgconfig:${libsecret.dev}/lib/pkgconfig:$PKG_CONFIG_PATH
   '';
 }
