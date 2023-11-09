0ad1e128a3617d85d062583d013b4f6c3ebaa7a5
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2023-11-09 00:47:36 -0300

    Add libthai to flutter with libsecret shell
    Signed-off-by: Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>

--- shells/flutter_with_libsecret.nix
+++ shells/flutter_with_libsecret.nix
@@ -14,9 +14,10 @@ mkShell {
     util-linux
     libselinux
     libsepol
+    libthai
   ];
 
   shellHook = ''
-    export PKG_CONFIG_PATH=${pcre.dev}/lib/pkgconfig:${libsepol}/lib/pkgconfig:${libselinux.dev}/lib/pkgconfig:${util-linux.dev}/lib/pkgconfig:${gtk3.dev}/lib/pkgconfig:${pcre2.dev}/lib/pkgconfig:${libsecret.dev}/lib/pkgconfig:$PKG_CONFIG_PATH
+    export PKG_CONFIG_PATH=${libthai.dev}/lib/pkgconfig:${pcre.dev}/lib/pkgconfig:${libsepol}/lib/pkgconfig:${libselinux.dev}/lib/pkgconfig:${util-linux.dev}/lib/pkgconfig:${gtk3.dev}/lib/pkgconfig:${pcre2.dev}/lib/pkgconfig:${libsecret.dev}/lib/pkgconfig:$PKG_CONFIG_PATH
   '';
 }
