71611a02c129568d6cc23691a72682a84190fc1f
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2023-11-09 00:56:58 -0300

    Add xorg.libXtst to flutter with libsecret shell
    Signed-off-by: Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>

--- shells/flutter_with_libsecret.nix
+++ shells/flutter_with_libsecret.nix
@@ -21,9 +21,10 @@ mkShell {
     xorg.libXdmcp
     libxkbcommon
     libepoxy
+    xorg.libXtst
   ];
 
   shellHook = ''
-    export PKG_CONFIG_PATH=${libepoxy.dev}/lib/pkgconfig:${libxkbcommon.dev}/lib/pkgconfig:${xorg.libXdmcp.dev}/lib/pkgconfig:${libgpg-error.dev}/lib/pkgconfig:${libdatrie.dev}/lib/pkgconfig:${libgcrypt.dev}/lib/pkgconfig:${libthai.dev}/lib/pkgconfig:${pcre.dev}/lib/pkgconfig:${libsepol}/lib/pkgconfig:${libselinux.dev}/lib/pkgconfig:${util-linux.dev}/lib/pkgconfig:${gtk3.dev}/lib/pkgconfig:${pcre2.dev}/lib/pkgconfig:${libsecret.dev}/lib/pkgconfig:$PKG_CONFIG_PATH
+    export PKG_CONFIG_PATH=${xorg.libXtst.dev}/lib/pkgconfig:${libepoxy.dev}/lib/pkgconfig:${libxkbcommon.dev}/lib/pkgconfig:${xorg.libXdmcp.dev}/lib/pkgconfig:${libgpg-error.dev}/lib/pkgconfig:${libdatrie.dev}/lib/pkgconfig:${libgcrypt.dev}/lib/pkgconfig:${libthai.dev}/lib/pkgconfig:${pcre.dev}/lib/pkgconfig:${libsepol}/lib/pkgconfig:${libselinux.dev}/lib/pkgconfig:${util-linux.dev}/lib/pkgconfig:${gtk3.dev}/lib/pkgconfig:${pcre2.dev}/lib/pkgconfig:${libsecret.dev}/lib/pkgconfig:$PKG_CONFIG_PATH
   '';
 }
