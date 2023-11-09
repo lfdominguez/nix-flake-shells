84db3aa43577c69d023b5dcb560bf3bcd06c49c1
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2023-11-09 00:41:37 -0300

    Add libselinux to flutter with libsecret shell
    Signed-off-by: Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>

--- shells/flutter_with_libsecret.nix
+++ shells/flutter_with_libsecret.nix
@@ -11,9 +11,10 @@ mkShell {
     pcre2
     gtk3
     util-linux
+    libselinux
   ];
 
   shellHook = ''
-    export PKG_CONFIG_PATH=${util-linux.dev}/lib/pkgconfig:${gtk3.dev}/lib/pkgconfig:${pcre2.dev}/lib/pkgconfig:${libsecret.dev}/lib/pkgconfig:$PKG_CONFIG_PATH
+    export PKG_CONFIG_PATH=${libselinux.dev}/lib/pkgconfig:${util-linux.dev}/lib/pkgconfig:${gtk3.dev}/lib/pkgconfig:${pcre2.dev}/lib/pkgconfig:${libsecret.dev}/lib/pkgconfig:$PKG_CONFIG_PATH
   '';
 }
