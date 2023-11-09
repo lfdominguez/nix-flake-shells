211080682bdf3c197fe912e704b1af0dc5f7cf97
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2023-11-09 00:34:23 -0300

    Add GTK 3 to flutter with libsecret shell
    Signed-off-by: Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>

--- shells/flutter_with_libsecret.nix
+++ shells/flutter_with_libsecret.nix
@@ -9,9 +9,10 @@ mkShell {
     pkg-config
     libsecret
     pcre2
+    gtk3
   ];
 
   shellHook = ''
-    export PKG_CONFIG_PATH=${pcre2.dev}/lib/pkgconfig:${libsecret.dev}/lib/pkgconfig:$PKG_CONFIG_PATH
+    export PKG_CONFIG_PATH=${gtk3.dev}/lib/pkgconfig:${pcre2.dev}/lib/pkgconfig:${libsecret.dev}/lib/pkgconfig:$PKG_CONFIG_PATH
   '';
 }
