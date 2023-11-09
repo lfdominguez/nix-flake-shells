81775cedeead1e2be7a53a8e97a457c0fb859d29
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2023-11-09 00:43:03 -0300

    Add libsepol to flutter with libsecret shell
    Signed-off-by: Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>

--- shells/flutter_with_libsecret.nix
+++ shells/flutter_with_libsecret.nix
@@ -12,9 +12,10 @@ mkShell {
     gtk3
     util-linux
     libselinux
+    libsepol
   ];
 
   shellHook = ''
-    export PKG_CONFIG_PATH=${libselinux.dev}/lib/pkgconfig:${util-linux.dev}/lib/pkgconfig:${gtk3.dev}/lib/pkgconfig:${pcre2.dev}/lib/pkgconfig:${libsecret.dev}/lib/pkgconfig:$PKG_CONFIG_PATH
+    export PKG_CONFIG_PATH=${libsepol}/lib/pkgconfig:${libselinux.dev}/lib/pkgconfig:${util-linux.dev}/lib/pkgconfig:${gtk3.dev}/lib/pkgconfig:${pcre2.dev}/lib/pkgconfig:${libsecret.dev}/lib/pkgconfig:$PKG_CONFIG_PATH
   '';
 }
