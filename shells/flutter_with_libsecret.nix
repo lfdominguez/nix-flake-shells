f54f2b7df27a5c4e9e78a608a814353054827889
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2023-11-09 00:53:04 -0300

    Add xorg.libXdmcp to flutter with libsecret shell
    Signed-off-by: Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>

--- shells/flutter_with_libsecret.nix
+++ shells/flutter_with_libsecret.nix
@@ -18,9 +18,10 @@ mkShell {
     libgcrypt
     libdatrie
     libgpg-error
+    xorg.libXdmcp
   ];
 
   shellHook = ''
-    export PKG_CONFIG_PATH=${libgpg-error.dev}/lib/pkgconfig:${libdatrie.dev}/lib/pkgconfig:${libgcrypt.dev}/lib/pkgconfig:${libthai.dev}/lib/pkgconfig:${pcre.dev}/lib/pkgconfig:${libsepol}/lib/pkgconfig:${libselinux.dev}/lib/pkgconfig:${util-linux.dev}/lib/pkgconfig:${gtk3.dev}/lib/pkgconfig:${pcre2.dev}/lib/pkgconfig:${libsecret.dev}/lib/pkgconfig:$PKG_CONFIG_PATH
+    export PKG_CONFIG_PATH=${xorg.libXdmcp.dev}/lib/pkgconfig:${libgpg-error.dev}/lib/pkgconfig:${libdatrie.dev}/lib/pkgconfig:${libgcrypt.dev}/lib/pkgconfig:${libthai.dev}/lib/pkgconfig:${pcre.dev}/lib/pkgconfig:${libsepol}/lib/pkgconfig:${libselinux.dev}/lib/pkgconfig:${util-linux.dev}/lib/pkgconfig:${gtk3.dev}/lib/pkgconfig:${pcre2.dev}/lib/pkgconfig:${libsecret.dev}/lib/pkgconfig:$PKG_CONFIG_PATH
   '';
 }
